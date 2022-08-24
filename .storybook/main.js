a977ea23a066dd6ee98e4e10167ab2566f69dc42
Manuel Guerrero <manu_pacu@hotmail.com>
2022-08-24 18:11:53 -0500

    Base test cases, base component

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../src/components/**/*.stories.@(js|jsx|ts|tsx)"],
   staticDirs: ["../public"],
   addons: [
     "@storybook/addon-links",
